fff514a6be372ecefe4e04eb7ea33d3183bfec2d
Amodh Dhakal <amodhakal@gmail.com>
2026-03-16 17:25:13 -0400

    fix: added service interface definitions
    Signed-off-by: Amodh Dhakal <amodhakal@gmail.com>

--- PolicyManager/Program.cs
+++ PolicyManager/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using PolicyManager.Data;
+using PolicyManager.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,7 +11,12 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IPolicyHoldersService, PolicyHoldersService>();
+builder.Services.AddScoped<IPoliciesService, PoliciesService>();
+builder.Services.AddScoped<IClaimsService, ClaimsService>();
 
 var app = builder.Build();
 
